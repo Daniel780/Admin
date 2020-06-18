e6e3936ef88c8120b028658e314c6127f0ec7dd8
SergioFernandez55 <sergiofv55@gmail.com>
2020-06-17 18:55:39 -0600

    Image upload WIP

---
+++ ImgTest.js
@@ -0,0 +1,16 @@
+class ImgTest {
+    constructor(name) {
+        this.name = name;
+        this.imgs = [];
+    }
+}
+
+
+function addImg(img){
+    alert("subiendo imagen");
+    imgs.push(img);
+}
+
+function getImg(num){
+    return imgs[num];
+}
